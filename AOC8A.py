b850057b4561cc3345f22315bcbd387f6f681f7d
Arpit Kumar Sinha <arpit22101@iiitnr.edu.in>
2023-12-08 15:57:19 +0530

    Create AOC8A.py

---
+++ AOC8A.py
@@ -0,0 +1,33 @@
+f1=open("AOC8.txt",'r')
+inst=str(f1.readline())
+inst=inst.strip('\n')
+inst=inst*60
+f1.readline()
+lines=f1.readlines()
+nodes=[]
+for i in lines:
+  l=i.split()
+  l.pop(1)
+  c=l[1]+l[2]
+  c=c.strip(')')
+  c=c.strip('(')
+  d=c.split(',')
+  l[1]=d[0]
+  l[2]=d[1]
+  nodes.append(l)
+steps=0
+node='AAA'
+count=0
+while(node!='ZZZ'):
+  for j in nodes:
+    if(j[0]==node):
+      if(inst[count]=='L'):
+        node=j[1]
+        steps+=1
+        break
+      elif(inst[count]=='R'):
+        node=j[2]
+        steps+=1
+        break
+  count+=1
+print(steps)
